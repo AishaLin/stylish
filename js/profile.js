d010f3e2fddc318d16946332813eb5f8ec249f58
Aisha Lin <aishalin314@gmail..com>
2019-03-06 00:41:30 +0800

    666

--- js/profile.js
+++ js/profile.js
@@ -6,9 +6,9 @@ const profile_email = document.querySelector('.profile_email')
 const profile_Content = document.querySelector('.profile_Content')
 //假如資料庫內的陣列有內容存在，執行以下的程式碼
 if (profile_arrayJason) {
-    consumer_photo.setAttribute('src', `${profile_arrayJason.picture}`)
-    profile_consumerName.textContent = profile_arrayJason.name
-    profile_email.textContent = profile_arrayJason.email
+    consumer_photo.setAttribute('src', `${profile_arrayJason[0].picture}`)
+    profile_consumerName.textContent = profile_arrayJason[0].name
+    profile_email.textContent = profile_arrayJason[0].email
 } else {
     profile_Content.innerHTML = '';
     profile_Content.innerHTML = '查無會員資料';
